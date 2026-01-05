cdd0bca41a9db4bc1c1e61c549f426a116c8174e
Ayser Choudhury <ayserchoudhury@gmail.com>
2026-01-05 17:28:01 +0900

    Updated contact section

--- src/content.js
+++ src/content.js
@@ -11,13 +11,18 @@ const siteContent = {
   terminal: {
     messages: [
       '',
+      '',
       "Hi, I'm Ayser 👋",
+      '',
       '',
       "I'm a Product Lead and Builder based in San Francisco, CA.",
       '',
+      '',
       'This site is a snapshot of who I am, how I think, what I build, and what I like.',
       '',
+      '',
       'Where should we start?',
+      '',
       '',
     ],
   },
